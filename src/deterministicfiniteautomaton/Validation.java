7f8518927667f4079e879bbcbd0a3007c3ee52f0
Cristian Penteado <31896473+cristianpenteado@users.noreply.github.com>
2019-03-26 23:31:13 -0300

    Update Validation.java

--- src/deterministicfiniteautomaton/Validation.java
+++ src/deterministicfiniteautomaton/Validation.java
@@ -2,10 +2,10 @@ package deterministicfiniteautomaton;
 
 public class Validation {
 
-    public boolean validateAlphabet(String valor) {
+    public boolean validateAlphabet(String value) {
 
         boolean returnValue = true;
-        char[] fragmentedValue = valor.toCharArray();
+        char[] fragmentedValue = value.toCharArray();
 
         for (int i = 0; i < fragmentedValue.length; i++) {
             if (fragmentedValue[i] != '0' && fragmentedValue[i] != '1') {
